af9f4bdc01a66d65ab00c55e0861f339ae7df272
Junkyu Park <flowershy@unionpos.co.kr>
2022-10-06 18:38:48 +0900

    OFFICE - 2022-10-06 PM18:37 [액티비티 메인]

---
+++ app/src/main/java/com/example/happynote/TitleActivity.kt
@@ -0,0 +1,27 @@
+package com.example.happynote
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.HandlerCompat.postDelayed
+
+class TitleActivity  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        moveMain(1)
+
+    }
+
+//    private fun moveMain(sec : Int) {
+//       Handler().postDelayed(object : Runnable, Any?, Long {
+//           override fun run() {
+//               TODO("Not yet implemented")
+//           }
+//
+//       })
+//    }
+
+}
